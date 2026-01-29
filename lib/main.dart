f90b0c87606b2ceb02fe542bc1dca3d4245e7ca9
passeggero23 <passeggero23@gmail.com>
2026-01-29 10:27:08 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,2 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(child: Text('UmanCapital App')),
-    ),
-  ));
-}
+void main() => runApp(MaterialApp(home: Scaffold(body: Center(child: Text('UmanCapital Ready')))));
