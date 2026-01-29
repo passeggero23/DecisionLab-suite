d9e1e712c1b0ddfc959d4d3b927d169de1ac2caa
passeggero23 <passeggero23@gmail.com>
2026-01-29 09:57:26 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,5 @@
-name: Build umancapital AAB
-on:
-  push:
-    branches: [ main ]
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v3
-      - uses: subosito/flutter-action@v2
-        with:
-          channel: 'stable'
-      - name: Install dependencies
-        run: flutter pub get
-      - name: Build AAB
-        run: flutter build appbundle --release
-      - name: Upload Artifact
-        uses: actions/upload-artifact@v3
-        with:
-          name: release-aab
-          path: build/app/outputs/bundle/release/app-release.aab
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Scaffold(body: Center(child: Text('UmanCapital')))));
+}
